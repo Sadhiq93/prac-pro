11b0e7e739ef891f341055ea895b02e131a8a676
Sadhiq93 <116905354+Sadhiq93@users.noreply.github.com>
2023-02-28 19:01:17 +0530

    Add files via upload

---
+++ mavenproject.groovy
@@ -0,0 +1,16 @@
+job ('first maven job dsl') {
+	description("this is dsl jon on ${new Date()}, this is small project hosted on github")
+	scm {
+		git("https://github.com/Sadhiq93/prac-pro.git" , 'master')
+		}
+	triggers {
+		scm('* * * * *')
+		}
+	steps{
+		maven('clean,package', 'prac-pro/blob/master/pom.xml')
+		}
+	publishers{
+		//archive this .war file generated
+		archiveArtifacts '**/*.war'
+		}
+	}
